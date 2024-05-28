72504744485d1cb76279c2377e5bf84a68afc22a
kermitcrayon <147984112+kermitcrayon@users.noreply.github.com>
2024-05-28 19:05:05 +0200

    Created folder for display code

---
+++ Source/display/Display.h
@@ -0,0 +1,59 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "ssd1306.h"
+
+/// @brief display class handles the display peripheral and shows screens for each state
+class Display 
+{
+public:
+	
+		Display();
+
+    // functions are static so they are accessible from the type (no object has to be initialized to access type)
+    static void displayInit();
+		static void showINITScreen();
+    static void showQRCODEScreen();
+    static void showSEARCHScreen();
+    /// @brief is the screen that shows up when the user presses the show direction button
+    static void showDIRECTIONSEARCHscreen();
+
+    static void showLeftArrow();
+    static void showRightArrow();
+    static void showUpArrow();
+    static void showDownArrow();
+
+    static void showS_HOT_COLDVeryColdStatus();
+    static void showS_HOT_COLDColdStatus();
+    static void showS_HOT_COLDWarmStatus();
+    static void showS_HOT_COLDHotStatus();
+
+    static void showS_HOT_COLDNotCloseAnymore();
+
+    static void showS_WAYPOINT_SimonSaysPuzzlePrompt();
+    static void showS_WAYPOINT_SimonSaysControlsTutorial();
+    static void showAwaitingUserInput();
+    static void showPuzzleLost();
+    static void showPuzzleWon();
+    // @brief shows a screen for n amount of seconds, and then returns to another screen
+    static void showScreenForNSeconds(int n, void (*screenToBeShown)(void), void(*screenToReturnTo) (void));
+    //  "Loading..." screen
+    static void showLoading();
+    // This screen shows up when the GPS is disconnected, so the user is prompted to find a better spot
+    static void showAwaitingReconnection();
+    static void showS_ENDGAMEGameEndedBecauseAllWaypointsWereReached();
+    // No reason for the error was given, check transitions to error state
+    static void showS_ERROR_NO_SOURCE();
+    static void showS_ERROR_ERROR_SENDING_DATA_AT_S_ENDGAME();
+    static void showS_ERROR_ERROR_DURING_GAME();
+    static void showS_ERROR_ERROR_INIT();
+
+    static void clearScreen();
+
+    //Still figuring out how to implement this class variable so that it will also be accessible in the ISR
+    //It stores the function pointer to the screenToReturnTo that was selected in showScreenForNSeconds
+		static void (*returnScreen)(void);
+
+private:
+};
+#endif
