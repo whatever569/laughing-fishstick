5e5e3da6deba0da95d7545514d17348a1a334292
Bars <ozguruzun.ltv@gmail.com>
2024-06-03 16:07:42 +0200

    cleaned up main

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,43 +9,6 @@ int main() {
 	eeprom_init();
 	millis_setup();
 	
-	/*
-	eeprom_write_string(EEPROM_currentAdress, "ninokuniW:2|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.34343, 343.43434C23.34|");
-	eeprom_write_string(EEPROM_currentAdress, "W0R1P1T40|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D52.34343, 102.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "D51.343443, 101.43434C26.34|");
-	eeprom_write_string(EEPROM_currentAdress, "W1R1P1T50|");
-	eeprom_write_string(EEPROM_currentAdress, "E55B3|");
-	*/
-	
-	EEPROM_currentAdress = 0x350;
-	GameDataReturn();
-	
 	delay_ms(100);
 	
 	while (1) {
